bd8b9321c07dd0cb0c514071ba9a874c1cdb9fd1
shwetanaik08 <112984589+shwetanaik08@users.noreply.github.com>
2024-07-30 12:49:07 +0530

    Update ColorCoder.h

--- ColorCoder.h
+++ ColorCoder.h
@@ -6,12 +6,8 @@ namespace TelCoColorCoder
     enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
     enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-    const char* MajorColorNames[] = {
-        "White", "Red", "Black", "Yellow", "Violet"
-    };
+    const char* MajorColorNames[];
 	
-    const char* MinorColorNames[] = {
-        "Blue", "Orange", "Green", "Brown", "Slate"
-    };
+    const char* MinorColorNames[];
 
 }
