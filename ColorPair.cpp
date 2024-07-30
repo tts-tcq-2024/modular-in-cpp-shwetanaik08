10a45f5e5de7d4b7feebac1a49f2f7b71d400559
shwetanaik08 <112984589+shwetanaik08@users.noreply.github.com>
2024-07-30 14:38:44 +0530

    Update ColorPair.cpp

--- ColorPair.cpp
+++ ColorPair.cpp
@@ -1,6 +1,6 @@
 #include "ColorPair.h"
 namespace TelCoColorCoder {
-    std::string ColorPair::ToString() const {
+    std::string ColorPair::ToString() {
         std::string colorPairStr = MajorColorNames[majorColor];
         colorPairStr += " ";
         colorPairStr += MinorColorNames[minorColor];
