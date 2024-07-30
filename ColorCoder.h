780ca4d3344a1695e32690ebca4cceabcb7da40d
shwetanaik08 <112984589+shwetanaik08@users.noreply.github.com>
2024-07-30 14:27:42 +0530

    Update ColorCoder.h

--- ColorCoder.h
+++ ColorCoder.h
@@ -1,23 +1,18 @@
 #ifndef COLORCODER_H
 #define COLORCODER_H
 
-#include <iostream>
-#include <assert.h>
-
-
 namespace TelCoColorCoder 
 {
     enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
     enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
     extern const char* MajorColorNames[];
-	
     extern const char* MinorColorNames[];
-
-    class ColorPair;
+    
+    class ColorPair; // Forward declaration of ColorPair class
 
     ColorPair GetColorFromPairNumber(int pairNumber);
     int GetPairNumberFromColor(MajorColor major, MinorColor minor);
-
 }
+
 #endif // COLORCODER_H
