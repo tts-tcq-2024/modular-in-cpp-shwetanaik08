1de15d824e903d2721d366f45494b7a59ba0f4d6
shwetanaik08 <112984589+shwetanaik08@users.noreply.github.com>
2024-07-30 11:53:36 +0530

    Create ColorPair.h

---
+++ ColorPair.h
@@ -0,0 +1,19 @@
+#include "ColorCoder.h"
+namespace TelCoColorCoder {
+class ColorPair {
+        private:
+            MajorColor majorColor;
+            MinorColor minorColor;
+        public:
+            ColorPair(MajorColor major, MinorColor minor):
+                majorColor(major), minorColor(minor)
+            {}
+            MajorColor getMajor() {
+                return majorColor;
+            }
+            MinorColor getMinor() {
+                return minorColor;
+            }
+            std::string ToString();
+    };
+}
